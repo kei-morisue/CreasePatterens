c6af7352b8a498c9c0eecea3eca795ae6e387db0
kei <morisue.kei@gmail.com>
2024-07-26 22:32:29 +0900

    add pics

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# CreasePatterens
-cp files source
+# CreasePatterensGallery
+CP images / source online viewer.
+
+Phitos taken by [Kei Morisue](https://x.com/keimorisue).
+
+PNG files rendered by [CPViewer](https://x.com/DamnGoodDay) by hAacHaMAcOokiNg.
+
+
+CP files exported / drawn by [Oriedita](https://github.com/oriedita/oriedita) by various.
+
+All rights concirning to the origami designs belongs to Kei Morisue.
--- src/main.js
+++ src/main.js
@@ -31,13 +31,22 @@ for (let j = 0; j < png_list.length; j++) {
     work_area.appendChild(title_area)
     const res = await async_img(png_list[j])
 
-    work_area.id = res.date
+    work_area.id = get_key(res)
     work_area.year = res.year
     title_area.innerHTML = res.title + "&nbsp;&nbsp;&nbsp;&nbsp;@" + res.date
+    title_area.style.fontWeight = 'bold'
 
     work_area.appendChild(res.img)
     work_area.style.display = "none"
 }
+for (let j = 0; j < pics_list.length; j++) {
+    const res = await async_img(pics_list[j])
+
+    const work_area = document.getElementById(get_key(res))
+    if (work_area == undefined || work_area == null) { debugger }
+    work_area.appendChild(res.img)
+}
+
 
 const filter_area = document.getElementById("filters")
 for (const [i, k] of years.entries()) {
@@ -65,6 +74,8 @@ for (const [i, k] of years.entries()) {
 }
 document.getElementById("2024").click()
 
+document.getElementById("load").style.display = "none"
+
 async function async_list(list_name) {
     return await fetch(list_name).then(
         responce => responce.text()
@@ -84,10 +95,14 @@ async function async_img(file_path) {
             const idx_path = file_path.indexOf("\\")
             const idx_ext = file_path.indexOf(".")
             const date = file_path.substr(idx_path + 1, 10)
-            const title = file_path.substring(idx_path + 11, idx_ext)
-            const title_no_under = title.replaceAll('_', ' ').toUpperCase().replaceAll("MORISUE KEI", "").replaceAll("CP", "")
+            const title = file_path.substring(idx_path + 11, idx_ext + 1)
+            const title_no_under = title.replaceAll("_cp.", "").replaceAll("_cp_", "_").replaceAll('_', ' ').replaceAll(".", "").toUpperCase().replaceAll("MORISUE KEI", "")
             const img_element = document.createElement('img');
             img_element.src = URL.createObjectURL(data);
             return { img: img_element, date: date, title: title_no_under, year: date.substring(0, 4) }
         })
+}
+
+function get_key(res) {
+    return res.date + res.title.substr(0, Math.min(4, res.title.length))
 }
